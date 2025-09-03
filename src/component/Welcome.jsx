7e1f38df7459e3ab19f92b0bb440bbf194f04cdf
shaik-sadhiks007 <shaik.sadhiks007@gmail.com>
2025-09-03 19:22:48 +0530

    react js syntax explained

---
+++ src/component/Welcome.jsx
@@ -0,0 +1,11 @@
+
+function Welcome() {
+    return (
+        <div>
+            <h1>welcome function code displaying</h1>
+            <h2>testing done</h2>
+        </div>
+    )
+}
+
+export default Welcome
